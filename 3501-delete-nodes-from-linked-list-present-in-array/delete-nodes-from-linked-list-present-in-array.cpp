d9a79589af80820d136213d5387733a066d039f8
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2025-11-01 05:42:43 +0530

    Time: 92 ms (79.13%) | Memory: 263.2 MB (86.67%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,21 +1,48 @@
-#include <unordered_set>
-#include <vector>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* modifiedList(std::vector<int>& nums, ListNode* head) {
+    bool is_inside(const std::unordered_set<int> &nums, int target){
         std::unordered_set<int> nums_set(nums.begin(), nums.end());
-        ListNode dummy(-1);
-        dummy.next = head;
-        ListNode* prev = &dummy;
-        ListNode* current = head;
-        while (current) {
-            if (nums_set.find(current->val) != nums_set.end()) {
-                prev->next = current->next;
-            } else {
-                prev = current;
-            }
-            current = current->next;
-        }
-        return dummy.next;
+        return nums_set.find(target) != nums_set.end();
     }
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        if(head==NULL)
+            return NULL;
+            
+        ListNode *current= head;
+        ListNode *prev= NULL;
+        std::unordered_set<int> nums_set(nums.begin(), nums.end());
+        while(current && current->next!=NULL){
+            if(nums_set.find(current->val) != nums_set.end()){
+                if(head==current){
+                    head=current->next;
+                    current=head;
+                }else{
+                    prev->next=current->next;
+                    current=prev->next;
+                }
+
+            }else{
+                prev=current;
+                current=current->next;
+            }
+        }
+        if(current && nums_set.find(current->val) != nums_set.end()){
+            prev->next=NULL;
+            delete current;
+        }
+        return head;
+ 
+           
+    } 
+
 };
