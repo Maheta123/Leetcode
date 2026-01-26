3b4e6ab2b74aa072353422f5810d53372b94d173
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2026-01-25 18:12:06 -0800

    Time: 23 ms (83.33%) | Memory: 20 MB (93.33%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.c
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.c
@@ -1,34 +1,48 @@
-int compare(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
-}
-int** minimumAbsDifference(int* arr, int arrSize, int* returnSize, int** returnColumnSizes) {
-    qsort(arr, arrSize, sizeof(int), compare);
-    
-    int minDiff = 2147483647;
-    for (int i = 1; i < arrSize; i++) {
-        int diff = arr[i] - arr[i - 1];
-        if (diff < minDiff) minDiff = diff;
-    }
-    
-    int count = 0;
-    for (int i = 1; i < arrSize; i++) {
-        if (arr[i] - arr[i - 1] == minDiff) count++;
-    }
-    
-    int** result = (int**)malloc(count * sizeof(int*));
-    *returnColumnSizes = (int*)malloc(count * sizeof(int));
-    *returnSize = count;
-    
-    int idx = 0;
-    for (int i = 1; i < arrSize; i++) {
-        if (arr[i] - arr[i - 1] == minDiff) {
-            result[idx] = (int*)malloc(2 * sizeof(int));
-            result[idx][0] = arr[i - 1];
-            result[idx][1] = arr[i];
-            (*returnColumnSizes)[idx] = 2;
-            idx++;
+
+int partition(int* arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low-1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
         }
     }
-    
-    return result;
+    i++;
+    int temp = arr[high];
+    arr[high] = arr[i];
+    arr[i] = temp;
+    return i;
+}
+
+void quicksort(int* arr, int low, int high) {
+    if (low < high) {
+        int part = partition(arr, low, high);
+        quicksort(arr, low, part-1);
+        quicksort(arr, part+1, high);
+    }
+}
+
+int** minimumAbsDifference(int* arr, int arrSize, int* returnSize, int** returnColumnSizes) {
+    quicksort(arr, 0, arrSize-1);
+    int mindiff = INT_MAX;
+    for (int i = 1; i < arrSize; i++) {
+        if (arr[i] - arr[i-1] < mindiff) mindiff = arr[i]-arr[i-1];
+    }
+    int** answer = (int**)malloc(arrSize * sizeof(int*));
+    *returnColumnSizes = malloc(arrSize * sizeof(int));
+    int pos = 0;
+    for (int i = 1; i < arrSize; i++) {
+        if (arr[i]-arr[i-1] == mindiff) {
+            answer[pos] = malloc(2 * sizeof(int));
+            answer[pos][0] = arr[i-1];
+            answer[pos][1] = arr[i];
+            (*returnColumnSizes)[pos] = 2;
+            pos++;
+        }
+    }
+    *returnSize = pos;
+    return answer;
 }
