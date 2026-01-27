622a40d415873fe5953b7dcc86b66360b3e25992
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2026-01-26 20:10:36 -0800

    Time: 348 ms (100.00%) | Memory: 67.4 MB (93.32%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
@@ -1,31 +1,31 @@
 class Solution:
-    def dijkstra(self, n: int) -> int:
-        INF  = 10**9
-        vis  = [False] * n
-        dist = [INF] * n
-
-        pq: List[Tuple[int, int]] = [(0, 0)]
-        dist[0] = 0
-
-        while pq:
-            du, u = heapq.heappop(pq)
-            if vis[u]:
-                continue
-            vis[u] = True
-
-            for v, w in self.G[u]:
-                nd = du + w
-                if nd < dist[v]:
-                    dist[v] = nd
-                    heapq.heappush(pq, (nd, v))
-
-        return -1 if dist[n - 1] == INF else dist[n - 1]
-
     def minCost(self, n: int, edges: List[List[int]]) -> int:
-        self.G = [[] for _ in range(n)]
-
+        graph = [[] for _ in range(n)]
         for u, v, w in edges:
-            self.G[u].append((v, w))
-            self.G[v].append((u, 2 * w))
+            graph[u].append((v, w))
+            graph[v].append((u, 2 * w))
 
-        return self.dijkstra(n)
+        dist = [float("inf")] * n
+        visited = [False] * n
+        dist[0] = 0
+        heap = [(0, 0)]
+        while heap:
+            cur_dist, u = heapq.heappop(heap)
+            if u == n - 1:
+                return cur_dist
+
+            if visited[u]:
+                continue
+            visited[u] = True
+
+            for v, w in graph[u]:
+                new_dist = cur_dist + w
+                if new_dist < dist[v]:
+                    dist[v] = new_dist
+                    heapq.heappush(heap, (new_dist, v))
+
+        return -1
+        # m = the number of edges
+        # n = the number of vertices
+        # time: O(n + mlogm)
+        # space: O(n + m)
