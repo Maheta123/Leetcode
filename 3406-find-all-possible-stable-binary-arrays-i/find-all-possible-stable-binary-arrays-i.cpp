a95485911c76459e0f2ee510948ab3184541d2d8
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2026-03-08 19:05:12 -0700

    Time: 0 ms (100.00%) | Memory: 8.9 MB (71.54%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -1,40 +1,26 @@
-class Solution 
-{
-private:
-    int m_limit;
+class Solution {
     const int MOD = 1e9 + 7;
-    vector<vector<vector<int>>> dp;
 public:
-    int numberOfStableArrays(int zero, int one, int limit) 
-    {
-        m_limit = limit;
-       
-        // SOL-2: MEMOIZATION DP -- TOP DOWN DP.
-        dp = vector<vector<vector<int>>>(zero+1, vector<vector<int>>(one+1, vector<int>(3, -1)));
-        return memo(zero, one, -1) % MOD;        
-    }
+    int numberOfStableArrays(int zero, int one, int limit) {
+        int dp[zero + 1][one + 1][2];
+        memset(dp, 0, sizeof(dp));
+        for (int i = min(zero, limit); i >= 0; i--)
+            dp[i][0][0] = 1;
+        for (int j = min(one, limit); j >= 0; j--)
+            dp[0][j][1] = 1;
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                // append 0
+                dp[i][j][0] = (0LL + dp[i - 1][j][0] + dp[i - 1][j][1]
+                                - (i - 1 - limit >= 0 ? dp[i - 1 - limit][j][1] : 0)
+                                + MOD) % MOD;
 
-private:
-    // SOL-2: MEMOIZATION -- TOP DOWN DP
-    int memo(int zero, int one, int prev)
-    {
-        if ( zero == 0 && one == 0 )
-            return 1;
-        
-        if ( zero < 0 || one < 0 )
-            return 0;
-        
-        if ( dp[zero][one][prev+1] != -1 )
-            return dp[zero][one][prev+1];
-
-        int ans = 0;
-        for ( int i=1; i<=m_limit; i++ )
-        {
-            if ( prev == -1 || prev == 0 )
-                ans = (ans + memo(zero, one-i, 1)) % MOD;
-            if ( prev == -1 || prev == 1 )
-                ans = (ans + memo(zero-i, one, 0)) % MOD;
+                // append 1
+                dp[i][j][1] = (0LL + dp[i][j - 1][0] + dp[i][j - 1][1]
+                                - (j - 1 - limit >= 0 ? dp[i][j - 1 - limit][0] : 0)
+                                + MOD) % MOD;
+            }
         }
-        return dp[zero][one][prev+1] = ans % MOD;
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
     }
 };
