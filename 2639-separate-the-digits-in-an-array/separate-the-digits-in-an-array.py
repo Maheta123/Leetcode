b63d01f6b6260efe2f46e87cd29eb38c7ba5d1da
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2026-05-11 08:23:56 +0530

    Time: 3 ms (98.37%) | Memory: 12.6 MB (93.50%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def separateDigits(self, nums):
-        result = []
-        for num in nums:
-            # Convert number to string, then split into digits
-            digits = [int(ch)  for ch in str(num)]
-            result.extend(digits)
-        
+        result=[]
+        for n in nums:
+            lst=[]
+            while n!=0:
+                lst.append(n%10)
+                n//=10
+            lst.reverse()
+            result.extend(lst)
         return result
-
-        
