8dc1941edb6ae290833faea1ff529398011c1982
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2026-05-15 14:21:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (73.62%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def majorityElement(self, nums):
-        nums.sort()
-        n = len(nums)
-        return nums[n/2]
-
+       nums.sort()
+       n = len(nums)
+       return nums[n/2]
         
