5098d6a14e7721d2ff01b671a2b3951112c140f5
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2026-05-18 02:45:11 +0530

    Time: 3 ms (29.98%) | Memory: 9.9 MB (77.12%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    bool isAlplaNumaric(char ch){
-            if( (ch>='0' && ch<='9')   ||  ( tolower(ch)>='a' && tolower(ch)<='z' )  ){
-                    return true;
-            }
-        return false;
-    }
 
     bool isPalindrome(string s) {
         int l=0,n =s.length()-1;
 
         while(l <=n){
-                if( !isAlplaNumaric(s[n])){
+                if( !isalnum(s[n])){
                     n--; continue;
                 }
-                if( !isAlplaNumaric(s[l])){
+                if( !isalnum(s[l])){
                     l++; continue;
                 }
                 if( tolower(s[l]) != tolower(s[n]) ){
