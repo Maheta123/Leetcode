c87a803481a1a3580d8407dbe0c89f7016e7aa2e
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2026-05-17 11:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (99.99%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,15 +1,52 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
+        int low = 0;
+        int high = nums.size() - 1;
 
-        for(int i=0;i<n; i++){
-                if(nums[i]==target){
-                    return i;
-                }
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid])
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+
+            else if (nums[mid] < target && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
-
         return -1;
-       
     }
 };
+
+
+#pragma GCC optimize("Ofast")
+
+#include <iostream>
+
+static constexpr std::size_t max_align = alignof(std::max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static std::size_t pos = 0;
+
+void *operator new(const std::size_t size) {
+    const std::size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const std::size_t size) {
+    return operator new(size);
+}
+
+void operator delete(void *) noexcept {}
+
+void operator delete[](void *) noexcept {}
+
+void operator delete(void *, std::size_t) noexcept {}
+
+void operator delete[](void *, std::size_t) noexcept {}
