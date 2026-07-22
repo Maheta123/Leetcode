f721ea18eb0632003d7010cdfad7f04ed891df36
Maheta Pragnesh <152085078+Maheta123@users.noreply.github.com>
2026-07-22 11:21:14 +0530

    Time: 36 ms (96.77%) | Memory: 255.3 MB (90.32%) - LeetSync

--- 3804-maximize-active-section-with-trade-ii/maximize-active-section-with-trade-ii.cpp
+++ 3804-maximize-active-section-with-trade-ii/maximize-active-section-with-trade-ii.cpp
@@ -1,109 +1,58 @@
-class SegmentTree {
-public:
-    int n;
-    vector<int> arr,seg;
-    SegmentTree(vector<int> &nums) {
-        n=nums.size();
-        arr=nums;
-        seg.assign(4 * max(1,n),0);
-        if(n) build(1,0,n-1);
-    }
-    void build(int node, int l, int r) {
-        if(l==r){
-            seg[node]=arr[l];
-            return;
-        }
-        int mid=(l+r) >> 1;
-        build(node*2,l,mid);
-        build(node*2+1,mid+1,r);
-        seg[node]=max(seg[node*2],seg[node*2+1]);
-    }
-    int internalQuery(int node, int st, int en, int l, int r) {
-        if(l<=st && en<=r) return seg[node];
-        int mid=(st+en) >> 1;
-        int res=0;
-        if(mid>=l) res=max(res,internalQuery(node*2,st,mid,l,r));
-        if(r>mid) res=max(res,internalQuery(node*2+1,mid+1,en,l,r));
-        return res;
-    }
-    int query(int l, int r) {
-        if(l>r || n==0) return 0;
-        return internalQuery(1,0,n-1,l,r);
-    }
-};
+const int K = 17, MAXN = 1e5;
+int st[K + 1][MAXN];
+
+void build(auto& array) {
+	copy(array.begin(), array.end(), st[0]);
+
+	for(int i = 1; i <= K; i++)
+	    for(int j = 0; j + (1 << i) <= array.size(); j++)
+	        st[i][j] = max(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+}
+
+int query(int L, int R) {
+	int i = bit_width(unsigned(R - L + 1)) - 1;
+	return max(st[i][L], st[i][R - (1 << i) + 1]);
+}
+
 class Solution {
 public:
-    int segsz=0;
-    int lowerBound(vector<int> &v, int x) {
-        int l=0,r=segsz;
-        while(l<r) {
-            int m=(l+r) >> 1;
-            if(v[m]>=x) r=m;
-            else l=m+1;
+    vector<int> maxActiveSectionsAfterTrade(const string& s, vector<vector<int>>& queries) {
+        int n = s.size(), active = 0;
+        vector<pair<int, int>> zero;
+        vector<int> index(n);
+        for(int i = 0; i < n; i++) {
+        	if(s[i] == '0') {
+        		if(i > 0 && s[i - 1] == '0') zero.back().second++;
+        		else zero.push_back({i, 1});
+        	}else {
+        		active++;
+        	}
+        	index[i] = int(zero.size()) - 1;
         }
-        return l;
-    }
-    int upperBound(vector<int> &v, int x) {
-        int l=0,r=segsz;
-        while(l<r){
-            int m=(l+r) >>1;
-            if(v[m]<=x) l=m+1;
-            else r=m;
+        if(zero.empty()) return vector<int>(queries.size(), active);
+        
+        vector<int> gains(zero.size() - 1);
+        for(int i = zero.size() - 2; i >= 0; i--) {
+        	gains[i] = zero[i].second + zero[i + 1].second;
         }
-        return l;
-    }
-    vector<int> maxActiveSectionsAfterTrade(string s, vector<vector<int>> &q) {
-        int n =s.size();
-        int cnt1=0;
-        for(char c: s)
-            if(c=='1')
-                cnt1++;
-        vector<int> zeroBlocks, zeroLeft, zeroRight;
-        int idx=0;
-        while(idx<n){
-            int r=idx;
-            while(r<n && s[idx]==s[r]) r++;
-            int len=r-idx;
-            if(s[idx]=='0'){
-                zeroBlocks.push_back(len);
-                zeroLeft.push_back(idx);
-                zeroRight.push_back(r-1);
-            }
+        build(gains);
 
-            idx=r;
-        }
-        int m=zeroBlocks.size();
-        segsz=m;
+        vector<int> res(queries.size(), active);
+        for(int i = 0, sz = queries.size(); i < queries.size(); i++) {
+        	int L = queries[i][0], R = queries[i][1];
+        	int start = index[L] + 1, end = index[R] - (s[R] == '0');
+        	int cnt_left = index[L] == -1 ? -1 : (zero[index[L]].second - (L - zero[index[L]].first));
+    		int cnt_right = index[R] == -1 ? -1 : (R - zero[index[R]].first + 1);
 
-        vector<int> ans;
-        if(m<=1){
-            for(int i=0;i<q.size();i++)
-                ans.push_back(cnt1);
-            return ans;
+        	if(start < end)
+    			res[i] = max(res[i], active + query(start, end - 1));
+    		if(s[L] == '0' && s[R] == '0' && index[L] + 1 == index[R])
+    			res[i] = max(res[i], active + cnt_left + cnt_right);
+            if(s[L] == '0' && index[L] + 1 < index[R] + (s[R] == '1'))
+                res[i] = max(res[i], active + cnt_left + zero[index[L] + 1].second);
+            if(s[R] == '0' && index[L] < index[R] - 1)
+                res[i] = max(res[i], active + cnt_right + zero[index[R] - 1].second);
         }
-        vector<int> nums(m-1);
-        for(int i=0;i<m-1;i++)
-            nums[i] = zeroBlocks[i]+zeroBlocks[i+1];
-        SegmentTree st(nums);
-        for(int i=0;i<q.size();i++){
-            int l=q[i][0],r=q[i][1];
-            int lidx=lowerBound(zeroRight,l);
-            int ridx=upperBound(zeroLeft,r)-1;
-            if(lidx>m-1 || ridx<0 || lidx>=ridx){
-                ans.push_back(cnt1);
-                continue;
-            }
-            int leftLen=zeroRight[lidx]-max(zeroLeft[lidx],l)+1;
-            int rightLen=min(r,zeroRight[ridx])-zeroLeft[ridx]+1;
-            if(lidx+1==ridx){
-                ans.push_back(cnt1+leftLen+rightLen);
-                continue;
-            }
-            int leftContri=leftLen+zeroBlocks[lidx+1];
-            int rightContri=rightLen+zeroBlocks[ridx-1];
-            int middleContri=st.query(lidx+1,ridx-2);
-            ans.push_back(cnt1+max(leftContri,max(rightContri, middleContri)));
-        }
-        return ans;
+        return res;
     }
 };
